5574dc6a4cf527c06d5630fa02f23f98fc678778
touhidalam_09 <alam.touhid@sslwireless.com>
2024-11-28 18:16:38 +0600

    added drawer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,6 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return <Stack >
     <Stack.Screen name='(tab)' options={{ headerShown: false }} />
+    <Stack.Screen name='(drawer)' options={{ headerShown: false }} />
   </Stack>;
 }
